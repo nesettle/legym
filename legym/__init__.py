7a1cbb9937e56a595c7b8789d745ea54e94c686b
MrCai <1014305148@qq.com>
2021-12-19 19:06:22 +0800

    :construction: 未模块化的代码

---
+++ legym/__init__.py
@@ -0,0 +1,6 @@
+from .activities import LegymActivities
+from .activity import ActivityState, LegymActivity
+from .core import Legym
+from .exception import LegymException
+from .requester import LegymRequester
+from .response import LegymResponse
